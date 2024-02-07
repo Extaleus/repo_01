30b696615968026386604168b22ce23b5867b481
Extaleus <streetworkoutinlenino@gmail.com>
2024-02-07 03:27:38 +0300

    solo start

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,40 +1,40 @@
-import './../css/Header.css';
-import logo from './../logo.svg';
+import "./../css/Header.css";
+import logo from "./../logo.svg";
 
 function Header() {
   return (
-    <div className='Header'>
-      <div className='Brand'>
-        <img src={logo} className="Brand__logo" alt='b__logo' />
-        <b className='Brand__text'>Tarkov Travel Guide</b>
+    <div className="Header">
+      <div className="Brand">
+        <img src={logo} className="Brand__logo" alt="b__logo" />
+        <b className="Brand__text">Tarkov Travel Guide</b>
       </div>
-      <div className='Clocks'>
-        <b className='Clocks__Time'>Time</b>
-        <b className='Clocks__Icon'>Icon</b>
+      <div className="Clocks">
+        <b className="Clocks__Time">Time</b>
+        <b className="Clocks__Icon">Icon</b>
       </div>
-      <div className='dropdown_Maps'>
+      <div className="dropdown_Maps">
         <b>Maps</b>
       </div>
-      <div className='dropdown_Progression'>
+      <div className="dropdown_Progression">
         <b>Progression</b>
       </div>
-      <div className='dropdown_Calculators'>
+      <div className="dropdown_Calculators">
         <b>Calculators</b>
       </div>
-      <div className='dropdown_Guns_Ammo'>
+      <div className="dropdown_Guns_Ammo">
         <b>Guns/Ammo</b>
       </div>
-      <div className='dropdown_More'>
+      <div className="dropdown_More">
         <b>More</b>
       </div>
-      <div className='Login'>
-        <div className='Login__Language'>
+      <div className="Login">
+        <div className="Login__Language">
           <b>EN/RU </b>
         </div>
-        <div className='Login__Button'>
+        <div className="Login__Button">
           <b> Login</b>
         </div>
-        <div className='Login__Profile'>
+        <div className="Login__Profile">
           <b> Profile</b>
         </div>
       </div>
