cd7871454afebf753e25c4837a70de642a3a96c8
Extaleus <streetworkoutinlenino@gmail.com>
2024-02-07 03:46:23 +0300

    fixed solo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import './App.css';
-import Header from './components/Header';
-import Content from './components/Content';
-import Footer from './components/Footer';
+import "./App.css";
+import Header from "./components/Header";
+import Content from "./components/Content";
+import Footer from "./components/Footer";
 
 function App() {
   return (
